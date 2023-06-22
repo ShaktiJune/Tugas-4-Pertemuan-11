dc511e1f042fd6b1384e287fc140d85f34bff12f
ShaktiJune <81726156+ShaktiJune@users.noreply.github.com>
2023-06-22 12:16:42 +0700

    Add files via upload

---
+++ controllers/Biodata.php
@@ -0,0 +1,10 @@
+<?php
+
+class dataDiri{
+    public function index(){
+        echo "My Biodata<br>";
+        echo "Nama : Shakti Hartono Putra<br>";
+        echo "NIM : D111811082<br>";
+        echo "Kelas : IF_2018A<br>";
+    }
+}
