445422dbca03b865ceb4f140552b585b8e5bfadc
ShaktiJune <81726156+ShaktiJune@users.noreply.github.com>
2023-06-22 11:44:49 +0700

    Add files via upload

---
+++ app/App.php
@@ -0,0 +1,43 @@
+<?php
+
+class App 
+{
+    protected $controller = 'HomeController';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct()
+    {
+        $url = $this->parse_Url();
+
+        // Load the controller
+        if (file_exists('../app/controllers/' . ucfirst($url[0]) . 'Controller.php')) {
+            $this->controller = ucfirst($url[0]) . 'Controller';
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Load the method
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // Load the parameters
+        $this->params = $url ? array_values($url) : [];
+
+        // Call the controller method with parameters
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    public function parse_Url()
+    {
+        if (isset($_GET['url'])) {
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    }
+}
